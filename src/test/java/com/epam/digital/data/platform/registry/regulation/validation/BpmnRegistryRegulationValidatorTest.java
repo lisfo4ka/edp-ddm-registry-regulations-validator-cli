41eba766ad089af76c3560e6fd9b803ef5cac0bf
Maksym Kharchenko <maksym_kharchenko@epam.com>
2021-08-26 12:05:41 +0000

    [EDP-DDM-753] Increased test coverage for BPMN & DMN validation

--- src/test/java/com/epam/digital/data/platform/registry/regulation/validation/BpmnRegistryRegulationValidatorTest.java
+++ src/test/java/com/epam/digital/data/platform/registry/regulation/validation/BpmnRegistryRegulationValidatorTest.java
@@ -3,8 +3,12 @@ package com.epam.digital.data.platform.registry.regulation.validation;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.io.File;
+import org.camunda.bpm.model.bpmn.Bpmn;
+import org.camunda.bpm.model.xml.ModelValidationException;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.MockedStatic;
+import org.mockito.Mockito;
 
 public class BpmnRegistryRegulationValidatorTest {
 
@@ -25,10 +29,45 @@ public class BpmnRegistryRegulationValidatorTest {
   }
 
   @Test
-  public void shouldFailProcessValidationDueToMissingProcessId() {
+  public void shouldFailProcessParsingDueToMissingProcessId() {
     var brokenProcessFile = getFileFromClasspath("registry-regulation/broken/process-broken.bpmn");
 
     var errors = this.validator.validate(brokenProcessFile);
+
+    assertThat(errors.isEmpty()).isFalse();
+  }
+
+  @Test
+  public void shouldFailDueToProcessModelValidationIssue() {
+    var processFile = getFileFromClasspath("registry-regulation/correct/process.bpmn");
+    var bpmnModel = Bpmn.readModelFromFile(processFile);
+
+    try (MockedStatic<Bpmn> bpmn = Mockito.mockStatic(Bpmn.class)) {
+      bpmn.when(() -> Bpmn.readModelFromFile(processFile))
+          .thenReturn(bpmnModel);
+      bpmn.when(() -> Bpmn.validateModel(bpmnModel))
+          .thenThrow(new ModelValidationException());
+
+      var errors = this.validator.validate(processFile);
+
+      assertThat(errors.isEmpty()).isFalse();
+    }
+  }
+
+  @Test
+  public void shouldFailDueToUnsupportedFileExtension() {
+    File missingFile = new File("missing-file.bpmn");
+
+    var errors = this.validator.validate(missingFile);
+
+    assertThat(errors.isEmpty()).isFalse();
+  }
+
+  @Test
+  public void shouldFailDueToMissingFile() {
+    File unsupportedFile = new File("unsupported-file.xml");
+
+    var errors = this.validator.validate(unsupportedFile);
 
     assertThat(errors.isEmpty()).isFalse();
   }
--- src/test/java/com/epam/digital/data/platform/registry/regulation/validation/DmnRegistryRegulationValidatorTest.java
+++ src/test/java/com/epam/digital/data/platform/registry/regulation/validation/DmnRegistryRegulationValidatorTest.java
@@ -3,8 +3,12 @@ package com.epam.digital.data.platform.registry.regulation.validation;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.io.File;
+import org.camunda.bpm.model.dmn.Dmn;
+import org.camunda.bpm.model.xml.ModelValidationException;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.MockedStatic;
+import org.mockito.Mockito;
 
 public class DmnRegistryRegulationValidatorTest {
 
@@ -17,20 +21,37 @@ public class DmnRegistryRegulationValidatorTest {
 
   @Test
   public void shouldPassRuleValidation() {
-    var processFile = getFileFromClasspath("registry-regulation/correct/rule.dmn");
+    var ruleFile = getFileFromClasspath("registry-regulation/correct/rule.dmn");
 
-    var errors = this.validator.validate(processFile);
+    var errors = this.validator.validate(ruleFile);
 
     assertThat(errors.isEmpty()).isTrue();
   }
 
   @Test
-  public void shouldFailRuleValidation() {
-    var processFile = getFileFromClasspath("registry-regulation/broken/rule-broken.dmn");
+  public void shouldFailDueToRuleParsingIssue() {
+    var ruleFile = getFileFromClasspath("registry-regulation/broken/rule-broken.dmn");
 
-    var errors = this.validator.validate(processFile);
+    var errors = this.validator.validate(ruleFile);
 
     assertThat(errors.isEmpty()).isFalse();
+  }
+
+  @Test
+  public void shouldFailDueToRuleModelValidationIssue() {
+    var ruleFile = getFileFromClasspath("registry-regulation/correct/rule.dmn");
+    var dmnModel = Dmn.readModelFromFile(ruleFile);
+
+    try (MockedStatic<Dmn> bpmn = Mockito.mockStatic(Dmn.class)) {
+      bpmn.when(() -> Dmn.readModelFromFile(ruleFile))
+          .thenReturn(dmnModel);
+      bpmn.when(() -> Dmn.validateModel(dmnModel))
+          .thenThrow(new ModelValidationException());
+
+      var errors = this.validator.validate(ruleFile);
+
+      assertThat(errors.isEmpty()).isFalse();
+    }
   }
 
   private File getFileFromClasspath(String filePath) {
