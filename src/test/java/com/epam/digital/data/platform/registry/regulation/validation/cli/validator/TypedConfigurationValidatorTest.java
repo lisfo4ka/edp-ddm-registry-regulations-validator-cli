453fbc76c8a68097e0d6d16277f519f3200e2198
Maksym_Kharchenko <maksym_kharchenko@epam.com>
2021-09-06 16:35:00 +0300

    [EDP-DDM-9064] Increased test coverage

--- src/test/java/com/epam/digital/data/platform/registry/regulation/validation/cli/validator/TypedConfigurationValidatorTest.java
+++ src/test/java/com/epam/digital/data/platform/registry/regulation/validation/cli/validator/TypedConfigurationValidatorTest.java
@@ -1,5 +1,9 @@
 package com.epam.digital.data.platform.registry.regulation.validation.cli.validator;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.empty;
+import static org.hamcrest.core.Is.is;
+import static org.hamcrest.core.IsNot.not;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
@@ -9,13 +13,15 @@ import com.epam.digital.data.platform.registry.regulation.validation.cli.model.B
 import com.epam.digital.data.platform.registry.regulation.validation.cli.support.RegulationConfigurationLoader;
 import java.io.File;
 import java.io.IOException;
+import lombok.SneakyThrows;
 import org.junit.Test;
 
 public class TypedConfigurationValidatorTest {
 
   @Test
   @SuppressWarnings("unchecked")
-  public void shouldLoadTypedConfigAndValidate() throws IOException {
+  @SneakyThrows
+  public void shouldLoadTypedConfigAndValidate() {
     var someFile = new File("");
     var bpAuthConfiguration = new BpAuthConfiguration();
     var validationContext = ValidationContext.empty();
@@ -35,5 +41,24 @@ public class TypedConfigurationValidatorTest {
 
     verify(configurationLoader, times(1)).load(someFile, BpAuthConfiguration.class);
     verify(bpAuthFileValidator, times(1)).validate(bpAuthConfiguration, validationContext);
+  }
+
+  @Test
+  @SneakyThrows
+  public void shouldFailIfUnableToLoadFile() {
+    var someFile = new File("");
+    var validationContext = ValidationContext.empty();
+
+    var configurationLoader = mock(RegulationConfigurationLoader.class);
+    when(configurationLoader.load(someFile, BpAuthConfiguration.class)).thenThrow(IOException.class);
+
+    var validator = TypedConfigurationValidator.<BpAuthConfiguration>builder()
+        .configurationClass(BpAuthConfiguration.class)
+        .configurationLoader(configurationLoader)
+        .build();
+
+    var errors = validator.validate(someFile, validationContext);
+
+    assertThat(errors, is(not(empty())));
   }
 }
--- src/test/java/com/epam/digital/data/platform/registry/regulation/validation/cli/validator/file/EmptyFileValidatorTest.java
+++ src/test/java/com/epam/digital/data/platform/registry/regulation/validation/cli/validator/file/EmptyFileValidatorTest.java
@@ -4,17 +4,30 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.empty;
 import static org.hamcrest.core.Is.is;
 import static org.hamcrest.core.IsNot.not;
+import static org.mockito.Mockito.times;
 
 import com.epam.digital.data.platform.registry.regulation.validation.cli.model.RegulationFileType;
 import com.epam.digital.data.platform.registry.regulation.validation.cli.validator.ValidationContext;
+import com.google.common.io.Files;
 import java.io.File;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import org.junit.Before;
 import org.junit.Test;
+import org.mockito.MockedStatic;
+import org.mockito.Mockito;
 
 public class EmptyFileValidatorTest {
 
+  private EmptyFileValidator validator;
+
+  @Before
+  public void setUp() {
+    this.validator = new EmptyFileValidator();
+  }
+
   @Test
   public void shouldPassIfNonEmptyFile() {
-    var validator = new EmptyFileValidator();
     var nonEmptyFile = getFileFromClasspath("registry-regulation/correct/bp-auth.yml");
 
     var errors = validator.validate(nonEmptyFile, ValidationContext.of(RegulationFileType.BP_AUTH));
@@ -24,12 +37,24 @@ public class EmptyFileValidatorTest {
 
   @Test
   public void shouldFailIfEmptyFile() {
-    var validator = new EmptyFileValidator();
     var emptyFile = getFileFromClasspath("registry-regulation/empty/empty-file.json");
 
     var errors = validator.validate(emptyFile, ValidationContext.of(RegulationFileType.BP_AUTH));
 
     assertThat(errors, is(not(empty())));
+  }
+
+  @Test
+  public void shouldFailIfUnableToReadFile() {
+    var someFile = new File("some-file");
+    try (MockedStatic<Files> filesUtilMock = Mockito.mockStatic(Files.class)) {
+      filesUtilMock.when(() -> Files.readLines(someFile, StandardCharsets.UTF_8)).thenThrow(IOException.class);
+
+      var errors = validator.validate(someFile, ValidationContext.empty());
+
+      filesUtilMock.verify(times(1), () -> Files.readLines(someFile, StandardCharsets.UTF_8));
+      assertThat(errors, is(not(empty())));
+    }
   }
 
   private File getFileFromClasspath(String filePath) {
